8c8537003f15662bb2736859215df3c4f12ceeb0
Mr-Deee <merchantdaniel8@gmail.com>
2023-05-19 16:22:35 +0000

    homepage

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './src/home';
+import LogInScreen from './src/login';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen name=" " component={LogInScreen} />
+      <Stack.Screen name=" " component={HomeScreen} />
+   
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default App;
+
+
+
