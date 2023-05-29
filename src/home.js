76d4c325ea419098d221a07ad0480853229b5647
Mr-Deee <merchantdaniel8@gmail.com>
2023-05-29 17:11:22 +0000

    home container

--- src/home.js
+++ src/home.js
@@ -1,21 +1,47 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-
+import { Button,StyleSheet, View, Text } from 'react-native';
+import { AppBar } from "@react-native-material/core";
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center',backgroundColor:'white' }}>
+ <View  style={styles.projectscard}>
+
+
+
+
+</View>
 
       <Text>Home Screen</Text>
       {/* <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Home')}
       /> */}
-
+     
 
 
       
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: 'white'        
+},
+  projectscard:{
+    
+    backgroundColor: "",
+    shadowRadius: 30,
+    shadowColor:"black",
+    borderRadius: 10,
+    borderColor: "black",
+    marginLeft:30,
+    height:94,
+    width:109,
+    marginRight:30,
+    borderWidth: 1,
+  }
+})
 
 export default HomeScreen
