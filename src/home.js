1986f7e2ac1759401641286daec3eefb24edc57f
Mr-Deee <merchantdaniel8@gmail.com>
2023-06-16 09:07:15 +0000

    git

--- src/home.js
+++ src/home.js
@@ -1,53 +1,56 @@
 import * as React from "react";
-import { TouchableOpacity, StyleSheet, View, Text, Image } from "react-native";
+import Github from "./components/Github";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  ScrollView,
+  Text,
+  Image,
+} from "react-native";
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
+      <ScrollView>
+        <View style={styles.folderrow}>
+          <View style={styles.projectscard}>
+            <Image
+              source={require("../assets/logo.png")}
+              style={styles.logoimage}
+            />
 
+            <View style={styles.textbrief}>
+              <Text
+                style={{
+                  color: "#ADD8E6",
+                  flexWrap: "wrap",
+                  marginLeft: 10,
+                  marginRight: 6,
+                }}
+              >
+                This pain shows all projects
+              </Text>
+            </View>
 
-    <View><Text>Hi There</Text></View>
-
-      <View style={styles.folderrow}>
-        <View style={styles.projectscard}>
-          <Image
-            source={require("../assets/logo.png")}
-            style={styles.logoimage}
-          />
-
-          <View style= {styles.textbrief}>
-          <Text style={{color:"#ADD8E6",flexWrap:"wrap",marginLeft:10,marginRight:6}}>
-          This pain shows all projects</Text>
+            <TouchableOpacity
+              style={styles.projectbutton}
+              onPress={() => navigation.navigate("Home")}
+            >
+              <Text style={styles.buttonText}>{">"}</Text>
+            </TouchableOpacity>
           </View>
 
-          <TouchableOpacity
-            style={styles.projectbutton}
-            onPress={() => navigation.navigate("Home")}
-          >
-            <Text style={styles.buttonText}>{">"}</Text>
-          </TouchableOpacity>
+          <View style={styles.projectscard02}></View>
         </View>
 
         <View style={styles.projectscard02}></View>
 
-
-        
-        
-      </View>
-
-
-      
-
-
-      <View style={styles.projectscard02}></View>    
-        <View style={styles.projectscard02}></View>
-
-
-      {/* <Text>Home Screen</Text> */}
-      {/* <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Home')}
-      /> */}
+        <View style={styles.subsection}>     
+          <View style={{ flex: 1 }}> 
+          <Github /></View> 
+          </View>
+      </ScrollView>
     </View>
   );
 }
@@ -56,10 +59,9 @@ const styles = StyleSheet.create({
   background: {
     backgroundColor: "white",
   },
-  textbrief:{
-  marginTop:5,
-  alignContent: "center"
-
+  textbrief: {
+    marginTop: 5,
+    alignContent: "center",
   },
   logoimage: {
     alignContent: "center",
@@ -68,10 +70,10 @@ const styles = StyleSheet.create({
     marginBottom: 2,
   },
   projectbutton: {
-    backgroundColor:"white",
-    borderRadius:50,
+    backgroundColor: "white",
+    borderRadius: 50,
     color: "white",
-    marginLeft:120,
+    marginLeft: 120,
     width: 25,
     height: 25,
     marginTop: 20,
@@ -80,7 +82,7 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "#ADD8E6",
     fontSize: 20,
-   
+
     textAlign: "center",
   },
   folderrow: {
@@ -102,12 +104,26 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
 
+  subsection: {
+    marginLeft:2,
+    marginRight:2,
+    borderTopRightRadius: 50,
+    borderColor: "black",
+    backgroundColor: "black",
+    borderBottomLeftRadius: 1,
+    shadowColor: "black",
+    borderBottomRightRadius: 1,
+    borderTopLeftRadius: 50,
+    marginTop: 20,
+
+    padding: 180,
+    
+  },
+
   projectscard02: {
     marginTop: 20,
     backgroundColor: "#ADD8E6",
-
     shadowColor: "black",
-
     borderRadius: 20,
     borderColor: "white",
     marginLeft: 8,
--- src/login.js
+++ src/login.js
@@ -1,7 +1,16 @@
 import { Input } from "native-base";
-import { Button, View, TouchableOpacity, Text, StyleSheet,  ScrollView,  SafeAreaView,
+import {
+  Button,
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
   TouchableWithoutFeedback,
-  KeyboardAvoidingView } from "react-native";
+  KeyboardAvoidingView,
+} from "react-native";
+
 import { TextInput } from "react-native-gesture-handler";
 import React, { useState } from "react";
 
@@ -11,122 +20,121 @@ const LoginScreen = ({ navigation }) => {
   const [password, setPassword] = useState("");
 
   return (
-     
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+      keyboardVerticalOffset={64}
+    >
+      <ScrollView>
+        <View style={styles.logoview}>
+          <Text style={styles.txtlogo}>D|N</Text>
+        </View>
+        <View style={styles.content}>
+       
 
-    <KeyboardAvoidingView style={styles.container} behavior="padding" keyboardVerticalOffset={64}>
-     <ScrollView>
+          <View style={styles.containerstyle}>
+            <TextInput
+              style={styles.emailtextinput}
+              placeholder="Email"
+              placeholderTextColor="#003f5c"
+              onChangeText={(email) => setEmail(email)}
+            ></TextInput>
 
-  
+            <TextInput
+              style={styles.passwordtextinput}
+              placeholder="Password"
+              placeholderTextColor="#003f5c"
+              secureTextEntry={true}
+              onChangeText={(password) => setPassword(password)}
+            ></TextInput>
 
-    <View style={styles.logoview}><Text style={styles.txtlogo}>D|N</Text></View>
-    <View style={styles.content}>
-      <Text>LogIn Screen</Text>
+            <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+              <Text style={styles.doyoutextStyle}>
+                Do you have an Account? Register.
+              </Text>
+            </TouchableOpacity>
 
-      <View style={styles.containerstyle}>
-        <TextInput
-          style={styles.emailtextinput}
-          placeholder="Email"
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-        ></TextInput>
-
-        
-          <TextInput
-          style={styles.passwordtextinput}
-          placeholder="Password"
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        ></TextInput>
-
-
-<TouchableOpacity
-        
-        onPress={() => navigation.navigate("Signup")}
-      >
-        <Text style={styles.doyoutextStyle}>Do you have an Account? Register.</Text>
-      </TouchableOpacity>
-
-
-
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("Home")}
-      >
-        <Text style={styles.textStyle}>Login</Text>
-      </TouchableOpacity>
-
-      </View>
-
-   
-
-
-</View>
-
-    </ScrollView>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => navigation.navigate("Home")}
+            >
+              <Text style={styles.textStyle}>Login</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
-
-logoview:{
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex:1,
-paddingTop: 49,
-paddingBottom:40,
+  container: {
+    flex: 1,
   },
 
-  doyoutextStyle:{
-    top:130,
-    left: 70,
-    color:"black"
-
+  logoview: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    paddingTop: 39,
+    paddingBottom: 10,
   },
-  gotosignup:{
-    
-backgroundColor:"transparent",
 
-
+  doyoutextStyle: {
+    top: 80,
+       color: "black",
+       marginTop:0,
+       marginLeft:30,
+       marginRight:10,
+  },
+  gotosignup: {
+    backgroundColor: "transparent",
   },
 
   content: {
     flex: 1,
     padding: 40,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   emailtextinput: {
-    paddingTop:10,
-    borderRadius: 10,
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingHorizontal: 30,
-  },
- passwordtextinput: {
-  paddingTop:10,
-  borderRadius: 10,
-  height: 40,
-  borderColor: 'gray',
-  borderWidth: 1,
-  paddingHorizontal: 30,
-  },
+    marginTop: 40,
+    marginLeft: 20,
+    marginRight: 20,
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
 
+    borderRadius: 10,
+    height: 20,
+    borderColor: "gray",
+    borderWidth: 1,
+  },
+  passwordtextinput: {
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+
+    borderRadius: 10,
+    height: 20,
+    borderColor: "gray",
+    borderWidth: 1,
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+  },
 
   txtlogo: {
     fontSize: 50,
-    paddingTop:80,
+    paddingTop: 80,
     paddingBottom: 50,
   },
   button: {
-    alignItems: "center", 
-    justifyContent: "center", 
-    top: 180,
-    left:20,
-    right: 20,
-    width:300,
-
+    alignItems: "center",
+    justifyContent: "center",
+    top: 100,
+    marginLeft: 20,
+    marginRight: 20,
+  
     alignItems: "center",
     justifyContent: "center",
     paddingVertical: 12,
@@ -140,9 +148,9 @@ backgroundColor:"transparent",
   },
 
   containerstyle: {
-    top: 30,
-    marginLeft:30,
-   marginRight:30,
+ 
+    marginLeft: 30,
+    marginRight: 30,
     backgroundColor: "white",
     borderRadius: 30,
     borderColor: "black",
