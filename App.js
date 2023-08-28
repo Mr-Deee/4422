8bd5a504d88d3ffd19811258649be56bae542dcb
Mr-Deee <merchantdaniel8@gmail.com>
2023-08-28 09:04:15 +0000

    adding firebases

--- App.js
+++ App.js
@@ -5,9 +5,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './src/home';
 import LoginScreen from './src/login';
 import SignupScreen from './src/signup';
+import firebase from 'firebase';
 
 const Stack = createStackNavigator();
 
+
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const App = () => (
   <NavigationContainer>
